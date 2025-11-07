8e208e699b9e2c0c09bb13dce8a4b3ff97279965
ehanz12 <rhanssap@gmail.com>
2025-11-07 13:17:26 +0700

    departement

--- application/views/content/marketing/customer/customer_data.php
+++ application/views/content/marketing/customer/customer_data.php
@@ -55,7 +55,7 @@
                                                 </thead>
                                                 <tbody>
                                                     <?php
-                                                    $level = $this->session->userdata('level');
+                                                    $level = $this->session->userdata('departement');
                                                     $no = 1;
                                                     foreach ($result as $k) {
                                                     ?>
